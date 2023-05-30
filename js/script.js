65c19967a94129c580c4afac6b9ace540b8db647
Shist <shist.pupust@mail.ru>
2023-05-30 16:13:43 +0300

    Added date of promotion with the timer of remaining time on the page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", () => {
   const tabContentBlocks = document.querySelectorAll(".tabcontent");
   const tabContainer = document.querySelector(".tabheader__items");
   const tabs = document.querySelectorAll(".tabheader__item");
 
-  const hideAllTabContent = () => {
+  function hideAllTabContent() {
     tabContentBlocks.forEach((contentBlock) => {
       contentBlock.classList.remove("appeared-block", "fade-animation");
       contentBlock.classList.add("hidden-element");
@@ -11,13 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
     tabs.forEach((tab) => {
       tab.classList.remove("tabheader__item_active");
     });
-  };
+  }
 
-  const showTabContentById = (id = 0) => {
+  function showTabContentById(id = 0) {
     tabContentBlocks[id].classList.remove("hidden-element");
     tabContentBlocks[id].classList.add("appeared-block", "fade-animation");
     tabs[id].classList.add("tabheader__item_active");
-  };
+  }
 
   tabContainer.addEventListener("click", (event) => {
     const clickedTab = event.target;
@@ -34,4 +36,69 @@ document.addEventListener("DOMContentLoaded", () => {
 
   hideAllTabContent();
   showTabContentById();
+
+  // Timer
+
+  const targetFullDate = new Date(); // Today's date (this variable will be changed)
+  targetFullDate.setDate(targetFullDate.getDate() + 5); // Today's date + 5 days
+  targetFullDate.setHours(0, 0, 0, 0); // Today's date + 5 days, but with zero hours, minutes, seconds and milliseconds
+  const targetDateArr = targetFullDate.toLocaleDateString().split(".");
+  const targetDay = targetDateArr[0];
+  const targetMonth = targetDateArr[1];
+  const targetYear = targetDateArr[2];
+  const targetTimeArr = targetFullDate.toLocaleTimeString().split(":");
+  const targetHours = targetTimeArr[0];
+  const targetMins = targetTimeArr[1];
+
+  const labelWithTargetDate = document.querySelector(".promotion__target-date");
+  labelWithTargetDate.textContent = `Акция закончится: ${targetDay}.${targetMonth}.${targetYear}, в ${targetHours}:${targetMins}`;
+
+  function calcRemainingTime(deadline) {
+    const totalMilSecs = deadline.getTime() - Date.parse(new Date());
+    const remainDays = Math.floor(totalMilSecs / 86400000);
+    const remainHours = Math.floor((totalMilSecs / 3600000) % 24);
+    const remainMins = Math.floor((totalMilSecs / 60000) % 60);
+    const remainSecs = Math.floor((totalMilSecs / 1000) % 60);
+    return {
+      totalMilSecs,
+      remainDays,
+      remainHours,
+      remainMins,
+      remainSecs,
+    };
+  }
+
+  function addNeededZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return `${num}`;
+    }
+  }
+
+  function showTimerOnPage(timerSelector, deadline) {
+    const timer = document.querySelector(timerSelector);
+
+    const daysBlock = timer.querySelector("#days");
+    const hoursBlock = timer.querySelector("#hours");
+    const minsBlock = timer.querySelector("#minutes");
+    const secsBlock = timer.querySelector("#seconds");
+    updateTimer();
+    const timerUpdater = setInterval(updateTimer, 1000);
+
+    function updateTimer() {
+      const remainTimeObj = calcRemainingTime(deadline);
+
+      daysBlock.textContent = addNeededZero(remainTimeObj.remainDays);
+      hoursBlock.textContent = addNeededZero(remainTimeObj.remainHours);
+      minsBlock.textContent = addNeededZero(remainTimeObj.remainMins);
+      secsBlock.textContent = addNeededZero(remainTimeObj.remainSecs);
+
+      if (remainTimeObj.totalMilSecs <= 0) {
+        clearInterval(timerUpdater);
+      }
+    }
+  }
+
+  showTimerOnPage(".timer", targetFullDate);
 });
