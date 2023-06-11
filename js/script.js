bda8935c6815e181b53586be8c2f6d82bae8c901
Shist <shist.pupust@mail.ru>
2023-06-11 23:23:22 +0300

    Food menu cards are now loading by script that uses classes

--- js/script.js
+++ js/script.js
@@ -156,4 +156,75 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
   document.addEventListener("scroll", openModalWindowByScroll);
+
+  // Использование классов для карточек
+
+  class FoodMenuCard {
+    constructor(headline, description, price, imgSrc, imgAlt, parentSelector) {
+      this.headline = headline;
+      this.description = description;
+      this.price = price;
+      this.transferUAH = 27; // May be we will get this data from some services in future
+      this.changeToUAH();
+      this.imgSrc = imgSrc;
+      this.imgAlt = imgAlt;
+      this.parent = document.querySelector(parentSelector);
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transferUAH;
+    }
+
+    render() {
+      this.parent.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class="menu__item">
+          <img src=${this.imgSrc} alt=${this.imgAlt} />
+          <h3 class="menu__item-subtitle">${this.headline}</h3>
+          <div class="menu__item-descr">
+            ${this.description}
+          </div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+          </div>
+        </div>
+      `
+      );
+    }
+  }
+
+  new FoodMenuCard(
+    `Меню "Фитнес"`,
+    `Меню "Фитнес" - это новый подход к приготовлению блюд: больше
+    свежих овощей и фруктов. Продукт активных и здоровых людей. Это
+    абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+    9, // Dollars
+    "img/tabs/vegy.jpg",
+    "vegy",
+    ".menu__field .container"
+  ).render();
+  new FoodMenuCard(
+    `Меню “Премиум”`,
+    `В меню “Премиум” мы используем не только красивый дизайн упаковки,
+    но и качественное исполнение блюд. Красная рыба, морепродукты,
+    фрукты - ресторанное меню без похода в ресторан!`,
+    14, // Dollars
+    "img/tabs/elite.jpg",
+    "elite",
+    ".menu__field .container"
+  ).render();
+  new FoodMenuCard(
+    `Меню "Постное"`,
+    `Меню “Постное” - это тщательный подбор ингредиентов: полное
+    отсутствие продуктов животного происхождения, молоко из миндаля,
+    овса, кокоса или гречки, правильное количество белков за счет тофу
+    и импортных вегетарианских стейков.`,
+    21, // Dollars
+    "img/tabs/post.jpg",
+    "post",
+    ".menu__field .container"
+  ).render();
 });
